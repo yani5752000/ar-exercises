93964a74ff308fdb239799f5e11af8e36bd9f7d0
Farid Asadpour <yani5752000@yahoo.com>
2021-08-17 17:43:10 +0000

    store change

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.name = 'Tokyo'
+@store1.save
+
+
+
+
+# user = User.find_by(name: 'David')
+# user.name = 'Dave'
+# user.save
